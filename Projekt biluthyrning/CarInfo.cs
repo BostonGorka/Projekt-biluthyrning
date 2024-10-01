e45fa3beb07670e37c94181a56ab5bb79fa64abf
BostonGorka <eric.vilhelmsson@elev.ga.lbs.se>
2024-10-01 14:44:45 +0200

    2024-10-01

--- Projekt biluthyrning/CarInfo.cs
+++ Projekt biluthyrning/CarInfo.cs
@@ -6,5 +6,40 @@ using System.Threading.Tasks;
 
 namespace Projekt_biluthyrning {
 	internal class CarInfo : VehicleInfo {
+		protected int HorsePower;
+		protected double FuelConsumption;
+		protected int Mileage;
+
+		public CarInfo(int inputHorsePower, double inputFuelConsumption, int inputMileage, string inputCarType, string inputGearbox, string inputFueltype, string inputDrivetrain) : base(inputCarType, inputGearbox, inputFueltype, inputDrivetrain) {
+			inputHorsePower = HorsePower;
+			inputFuelConsumption = FuelConsumption;
+			inputMileage = Mileage;
+		}
+
+		List<CarInfo> ListOfAvaiableCars = new List<CarInfo>() {
+			new CarInfo(210, 0.88, 28000, "Sedan", "Manual", "Petrol", "FWD"),
+			new CarInfo(170, 0.7, 43000, "Combi", "Automatic", "Diesel", "RWD"),
+			new CarInfo(260, 0.7, 25000, "SUV", "Automatic", "Petrol", "AWD"),
+			new CarInfo(300, 0.95, 32000, "Sedan", "Automatic", "Petrol", "FWD"),
+			new CarInfo(200, 0.68, 19000, "Combi", "Automatic", "Diesel", "AWD"),
+			new CarInfo(400, 0, 12000, "SUV", "Automatic", "Electric", "AWD"),
+			new CarInfo(220, 1.15, 20000, "SUV", "Manual", "Petrol", "RWD"),
+			new CarInfo(170, 0.9, 36000, "Combi", "Manual", "CNG", "FWD"),
+		};
+
+		public List<CarInfo> MethodOfElimination(string inputCarType, string inputGearbox, string inputFueltype, string inputDrivetrain) {
+			List<CarInfo> AvailableCars = new List<CarInfo>();
+			for(int i = 0; i <= ListOfAvaiableCars.Count; i++) {
+				if (ListOfAvaiableCars[i].CarType == inputCarType && ListOfAvaiableCars[i].Gearbox == inputGearbox && ListOfAvaiableCars[i].Fueltype == inputFueltype && ListOfAvaiableCars[i].Drivetrain == inputDrivetrain) {
+					AvailableCars.Add(ListOfAvaiableCars[i]);
+				}
+			}
+			return AvailableCars;
+			
+		}
+		
+
+
+
 	}
 }
--- Projekt biluthyrning/UserInterface.cs
+++ Projekt biluthyrning/UserInterface.cs
@@ -8,5 +8,15 @@ namespace Projekt_biluthyrning {
 	internal class UserInterface {
 		VehicleInfo vehicleInfo;
 
+	   public void StartPage() {
+			Console.WriteLine("-----------------------------------------");
+			Console.WriteLine("Welcome, we have very good rental cars here at CarRentCenter.Com");
+			Console.WriteLine("--------------------------------------------");
+			Console.WriteLine("Press any key to continue to booking");
+			Console.ReadKey();
+
+			Console.Clear();
+
+		}
 	}
 }
